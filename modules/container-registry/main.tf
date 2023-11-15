fab08b63fb77a458e6b1dd9ac6b9bc6fe8fe5590
Dave Harding <daveharding@outlook.com>
2023-11-15 10:34:22 -0800

    Update main.tf

--- modules/container-registry/main.tf
+++ modules/container-registry/main.tf
@@ -1,6 +1,7 @@
 resource "azurerm_container_registry" "acr" {
   #checkov:skip=CKV_AZURE_139:Azure Container registries Public access to All networks is enabled
   #checkov:skip=CKV_AZURE_165:Ensure geo-replicated container registries to match multi-region container deployments
+  #checkov:skip=CKV_AZURE_167:Ensure a retention policy is set to cleanup untagged manifests
   name                = "${var.workspace}acr${var.environment}"
   resource_group_name = var.resource_group_name
   location            = var.location

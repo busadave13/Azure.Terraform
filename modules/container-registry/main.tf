e07fcf93606a5463883ebc5686720dd9a0edc702
Dave Harding <daveharding@outlook.com>
2023-11-15 11:18:19 -0800

    Update main.tf

--- modules/container-registry/main.tf
+++ modules/container-registry/main.tf
@@ -3,6 +3,7 @@ resource "azurerm_container_registry" "acr" {
   #checkov:skip=CKV_AZURE_165:Ensure geo-replicated container registries to match multi-region container deployments
   #checkov:skip=CKV_AZURE_167:Ensure a retention policy is set to cleanup untagged manifests
   #checkov:skip=CKV_AZURE_166:Ensure container image quarantine, scan, and mark images verified
+  #checkov:skip=CKV_AZURE_164:Ensures that ACR uses signed/trusted images
   name                = "${var.workspace}acr${var.environment}"
   resource_group_name = var.resource_group_name
   location            = var.location

4192a6da4a9e6a7d0df043a05194963688153506
Dave Harding <daveharding@outlook.com>
2023-11-15 10:32:45 -0800

    Update main.tf

--- modules/container-registry/main.tf
+++ modules/container-registry/main.tf
@@ -1,7 +1,6 @@
 resource "azurerm_container_registry" "acr" {
-  #checkov:skip=CKV_AZURE_139:Azure Container registries Public access to All networks is enabled.
-  #checkov:skip=CKV_AZURE_165:Ensure geo-replicated container registries to match multi-region container deployments.
-  #checkov:skip=CKV_AZURE_166:Ensure container image quarantine, scan, and mark images verified
+  #checkov:skip=CKV_AZURE_139:Azure Container registries Public access to All networks is enabled
+  #checkov:skip=CKV_AZURE_165:Ensure geo-replicated container registries to match multi-region container deployments
   name                = "${var.workspace}acr${var.environment}"
   resource_group_name = var.resource_group_name
   location            = var.location
@@ -10,11 +9,11 @@ resource "azurerm_container_registry" "acr" {
   trust_policy {
     enabled = true
   }
-  retention_policy {
-    enabled = true
-    days    = 7
-  }
-  quarantine_policy_enabled     = false
+ # retention_policy {
+ #   enabled = true
+ #   days    = 7
+ # }
+  quarantine_policy_enabled     = true
   public_network_access_enabled = false
   tags                          = var.tags
 }

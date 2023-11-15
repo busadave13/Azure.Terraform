dd01439f372c36965f15e07d56aa68abcc37ef0f
Dave Harding <daveharding@outlook.com>
2023-11-15 09:30:40 -0800

    Latest changes

--- modules/container-registry/main.tf
+++ modules/container-registry/main.tf
@@ -1,17 +1,18 @@
 resource "azurerm_container_registry" "acr" {
+  #checkov:skip=CKV_AZURE_139:Azure Container registries Public access to All networks is enabled
   name                = "${var.workspace}acr${var.environment}"
-  resource_group_name = var.resource-group-name
+  resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Standard"
-  admin_enabled       = true
-
-  tags = var.tags
-}
-
-output "container_registry_id" {
-  value = azurerm_container_registry.acr.id
-}
-
-output "container_registry_name" {
-  value = azurerm_container_registry.acr.name
+  admin_enabled       = false
+  trust_policy {
+    enabled = true
+  }
+  retention_policy {
+    enabled = true
+    days    = 7
+  }
+  quarantine_policy_enabled     = true
+  public_network_access_enabled = false
+  tags                          = var.tags
 }
---
+++ modules/container-registry/outputs.tf
@@ -0,0 +1,8 @@
+
+output "container_registry_id" {
+  value = azurerm_container_registry.acr.id
+}
+
+output "container_registry_name" {
+  value = azurerm_container_registry.acr.name
+}
